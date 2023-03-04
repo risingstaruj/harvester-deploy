c7179ae6273aedfbe56c0fb111305bef9e40f9b8
한의진 <risingstaruj@gmail.com>
2023-03-04 23:56:36 +0900

    First Commit - Repo created and Set

---
+++ src/components/Sidebar.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const BarDiv = styled.div`
+    background-color: #161717;
+    width: 100px;
+    height: 100%; 
+    position: absolute;
+    top: 0px;
+    
+`
+
+const ChartsImg = styled.image`
+    position: absolute;
+    
+`
+function Sidebar()
+{  
+    return(
+        <BarDiv>
+            
+        </BarDiv>
+    );   
+
+
+}
+
+export default Sidebar;
