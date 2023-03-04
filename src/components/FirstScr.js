41e32d68f9409b1555eba97d87a371cf88f43ce9
한의진 <risingstaruj@gmail.com>
2023-03-05 00:59:30 +0900

    메인화면 제목 및 내용부분 레이아웃 구성 완료

--- src/components/FirstScr.js
+++ src/components/FirstScr.js
@@ -8,11 +8,39 @@ const BackDiv = styled.div`
     
 `
 
+const FirstContent = styled.div`
+    position: absolute;
+    background-color: #E3E3E3;
+    left: 25vw;
+    top: 276px;
+    width: 60vw;
+    height: 56vh;
+
+    border-radius: 17px;
+`
+const HarvestText = styled.div`
+    position: absolute;
+    left: 137px;
+    color: #ffffff;
+    font-family: 'Qwigley';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 10vw;
+    line-height: 125px;
+
+
+    letter-spacing: 0.03em;
+
+`
+
 
 function FirstScr()
 {
     return(
-        <BackDiv></BackDiv>
+        <BackDiv>
+            <HarvestText>Harvest Demo</HarvestText>
+            <FirstContent></FirstContent>
+        </BackDiv>
     );
 }
 
