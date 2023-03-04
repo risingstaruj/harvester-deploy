421bc0c219293cb7ca1656f604395e8de7474d69
한의진 <risingstaruj@gmail.com>
2023-03-05 00:43:09 +0900

    Main화면 배경 구성 완료

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import Sidebar from './components/Sidebar';
+import FirstScr from './components/FirstScr';
 import styled from 'styled-components';
-
+import styles from './BackScreen.module.css';
 
 function App() {
   return (
     <div className="App">
      <div><Sidebar /></div>
-        
+        <div className={styles.BackScreen}>
+          <FirstScr></FirstScr>
+        </div>
       
     </div>
   );
---
+++ src/components/FirstScr.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const BackDiv = styled.div`
+    background-color: #1E1E1E;
+    height: 100vh;
+    
+    
+`
+
+
+function FirstScr()
+{
+    return(
+        <BackDiv></BackDiv>
+    );
+}
+
+export default FirstScr;
