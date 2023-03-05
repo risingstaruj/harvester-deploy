7cd4ee52c7ad6266e4da64834bdd65a942ce69d0
한의진 <risingstaruj@gmail.com>
2023-03-05 22:48:37 +0900

    SideBar 모든 아이콘 삽입 완료

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -15,6 +15,36 @@ const ChartsImg = styled.div`
     top: 36px;
     left: 25px;
 `
+
+const YoutubeSideImage = styled.div`
+    position: absolute;
+    top: 120px;
+    padding-top: 20px;
+    padding-bottom: 20px;
+    padding-left: 25px;
+    padding-right: 27px;
+    border-radius: 7px;
+    background-color: #000000;
+`
+const NaverCafeImg = styled.div`
+    position: absolute;
+    top: 210px;
+    left: 21px;
+`
+
+const InstagramImg = styled.div`
+    position: absolute;
+    top: 290px;
+    left: 31.2px;
+`
+const NaverImg = styled.div`
+    position: absolute;
+    top: 354px;
+    left: 30px;
+`
+
+
+
 function Sidebar()
 {  
     return(
@@ -23,7 +53,20 @@ function Sidebar()
                 <img src={require("../static/image/Frame.png")}>
                     </img>
                 </ChartsImg>
+                <YoutubeSideImage>
+                    <img src={require("../static/image/Youtube.png")}></img>
+                </YoutubeSideImage>
+                <NaverCafeImg>
+                    <img src={require("../static/image/naver cafe.png")}></img>
+                </NaverCafeImg>
+                <InstagramImg>
+                    <img src={require("../static/image/instagram.png")}></img>
+                </InstagramImg>
+                <NaverImg>
+                    <img src={require("../static/image/Naver.png")}></img>
+                </NaverImg>
         </BarDiv>
+
     );   
 
 
